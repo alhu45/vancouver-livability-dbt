6e04a04a9c3804a6f33a1206bcf9852a7e6a1720
Alan <17.alanhu@gmail.com>
2026-04-07 15:42:44 +0000

    debugging

--- models/staging/stg_transit_stops.sql
+++ models/staging/stg_transit_stops.sql
@@ -102,4 +102,90 @@ SELECT
         ELSE 'Outside Vancouver'
     END AS neighbourhood
 
-FROM VANCOUVER_DATA.SILVER.TRANSIT_STOPS_CLEAN
+FROM VANCOUVER_DATA.S{{ config(
+    materialized = 'view',
+    schema = 'SILVER'
+) }}
+
+SELECT
+    stop_id,
+    stop_code,
+    stop_name,
+    stop_desc,
+    stop_lat,
+    stop_lon,
+    zone_id,
+    stop_url,
+    location_type,
+    parent_station,
+    wheelchair_boarding,
+    CASE
+        WHEN stop_lat BETWEEN 49.279 AND 49.290
+             AND stop_lon BETWEEN -123.135 AND -123.110
+             THEN 'Downtown'
+        WHEN stop_lat BETWEEN 49.279 AND 49.295
+             AND stop_lon BETWEEN -123.155 AND -123.135
+             THEN 'West End'
+        WHEN stop_lat BETWEEN 49.270 AND 49.285
+             AND stop_lon BETWEEN -123.095 AND -123.070
+             THEN 'Strathcona'
+        WHEN stop_lat BETWEEN 49.265 AND 49.282
+             AND stop_lon BETWEEN -123.075 AND -123.050
+             THEN 'Grandview-Woodland'
+        WHEN stop_lat BETWEEN 49.270 AND 49.290
+             AND stop_lon BETWEEN -123.055 AND -123.020
+             THEN 'Hastings-Sunrise'
+        WHEN stop_lat BETWEEN 49.265 AND 49.285
+             AND stop_lon BETWEEN -123.220 AND -123.175
+             THEN 'West Point Grey'
+        WHEN stop_lat BETWEEN 49.260 AND 49.279
+             AND stop_lon BETWEEN -123.175 AND -123.140
+             THEN 'Kitsilano'
+        WHEN stop_lat BETWEEN 49.260 AND 49.279
+             AND stop_lon BETWEEN -123.140 AND -123.115
+             THEN 'Fairview'
+        WHEN stop_lat BETWEEN 49.255 AND 49.275
+             AND stop_lon BETWEEN -123.115 AND -123.085
+             THEN 'Mount Pleasant'
+        WHEN stop_lat BETWEEN 49.235 AND 49.265
+             AND stop_lon BETWEEN -123.220 AND -123.175
+             THEN 'Dunbar-Southlands'
+        WHEN stop_lat BETWEEN 49.240 AND 49.265
+             AND stop_lon BETWEEN -123.175 AND -123.150
+             THEN 'Arbutus-Ridge'
+        WHEN stop_lat BETWEEN 49.240 AND 49.265
+             AND stop_lon BETWEEN -123.150 AND -123.125
+             THEN 'Shaughnessy'
+        WHEN stop_lat BETWEEN 49.240 AND 49.265
+             AND stop_lon BETWEEN -123.125 AND -123.110
+             THEN 'South Cambie'
+        WHEN stop_lat BETWEEN 49.240 AND 49.262
+             AND stop_lon BETWEEN -123.110 AND -123.090
+             THEN 'Riley Park'
+        WHEN stop_lat BETWEEN 49.240 AND 49.262
+             AND stop_lon BETWEEN -123.090 AND -123.060
+             THEN 'Kensington-Cedar Cottage'
+        WHEN stop_lat BETWEEN 49.240 AND 49.270
+             AND stop_lon BETWEEN -123.060 AND -123.030
+             THEN 'Renfrew-Collingwood'
+        WHEN stop_lat BETWEEN 49.220 AND 49.245
+             AND stop_lon BETWEEN -123.175 AND -123.145
+             THEN 'Kerrisdale'
+        WHEN stop_lat BETWEEN 49.220 AND 49.245
+             AND stop_lon BETWEEN -123.145 AND -123.115
+             THEN 'Oakridge'
+        WHEN stop_lat BETWEEN 49.200 AND 49.225
+             AND stop_lon BETWEEN -123.145 AND -123.100
+             THEN 'Marpole'
+        WHEN stop_lat BETWEEN 49.220 AND 49.245
+             AND stop_lon BETWEEN -123.100 AND -123.060
+             THEN 'Sunset'
+        WHEN stop_lat BETWEEN 49.200 AND 49.228
+             AND stop_lon BETWEEN -123.060 AND -123.025
+             THEN 'Victoria-Fraserview'
+        WHEN stop_lat BETWEEN 49.200 AND 49.240
+             AND stop_lon BETWEEN -123.025 AND -122.990
+             THEN 'Killarney'
+        ELSE 'Outside Vancouver'
+    END AS neighbourhood
+FROM VANCOUVER_DATA.SILVER.TRANSIT_STOPS_CLEANILVER.TRANSIT_STOPS_CLEAN
