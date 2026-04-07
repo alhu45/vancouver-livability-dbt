0762a4cb1105256e3757438c80eb278cc36b7942
Alan <17.alanhu@gmail.com>
2026-04-07 17:36:03 +0000

    adding window functions to gold_transit_stop_activity

--- models/gold/gold_transit_stop_activity.sql
+++ models/gold/gold_transit_stop_activity.sql
@@ -15,11 +15,40 @@ WITH stop_trip_counts AS (
         COUNT(DISTINCT r.route_id)       AS total_routes_served,
         COUNT(DISTINCT st.arrival_time)  AS total_arrivals
     FROM {{ ref('stg_transit_stoptimes') }}  st
-    JOIN {{ ref('stg_transit_stops') }}       s  ON st."stop_id"  = s."stop_id"
-    JOIN {{ ref('stg_transit_trips') }}       t  ON st."trip_id"  = t."trip_id"
-    JOIN {{ ref('stg_transit_routes') }}      r  ON t."route_id"  = r."route_id"
+    JOIN {{ ref('stg_transit_stops') }}       s  ON st.stop_id  = s.stop_id
+    JOIN {{ ref('stg_transit_trips') }}       t  ON st.trip_id  = t.trip_id
+    JOIN {{ ref('stg_transit_routes') }}      r  ON t.route_id  = r.route_id
     WHERE s.neighbourhood != 'Outside Vancouver'
     GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
 )
 
-SELECT * FROM stop_trip_counts
+SELECT
+    neighbourhood,
+    stop_id,
+    stop_name,
+    stop_lat,
+    stop_lon,
+    wheelchair_boarding,
+    route_short_name,
+    route_long_name,
+    route_type,
+    total_trips,
+    total_routes_served,
+    total_arrivals,
+    RANK() OVER (
+        PARTITION BY neighbourhood
+        ORDER BY total_trips DESC
+    ) AS rank_within_neighbourhood,
+    RANK() OVER (
+        ORDER BY total_trips DESC
+    ) AS citywide_stop_rank,
+    ROUND(
+        total_trips * 100.0 /
+        NULLIF(SUM(total_trips) OVER (PARTITION BY neighbourhood), 0), 2
+    ) AS pct_of_neighbourhood_trips,
+    ROUND(
+        total_trips * 100.0 /
+        NULLIF(SUM(total_trips) OVER (), 0), 2
+    ) AS pct_of_city_trips
+FROM stop_trip_counts
+ORDER BY neighbourhood, total_trips DESC
